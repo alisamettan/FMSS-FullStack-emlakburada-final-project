59ff0b48ab98c1e385d5b313a7a21405d1954f17
alisamettan <alisamet_tan@hotmail.com>
2024-07-07 20:38:54 +0300

    incomplete folders added

---
+++ emlakburada-advertstatus/src/test/java/com/patika/emlakburada_advertstatus/EmlakburadaAdvertstatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patika.emlakburada_advertstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmlakburadaAdvertstatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
